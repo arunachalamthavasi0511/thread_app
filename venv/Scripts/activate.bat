8fe1656cdb05083ab992491b4808c69cfd9459ec
arunachalamthavasi0511 <arunachalamthavasi0511@gmail.com>
2025-12-07 13:39:15 +0530

    upload thread_app

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\My_test\Arunachalam\thread_app\venv
+set VIRTUAL_ENV=D:\My_test\Arunachalam\Thread_inventory_app_production\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
